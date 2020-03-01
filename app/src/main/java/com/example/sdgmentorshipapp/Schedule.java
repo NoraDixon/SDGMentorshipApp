78cb72daf1b8a08e5277df67e1095e16ea2b71a5
Clarekpd <Clarekpd@outlook.com>
2020-03-01 12:47:59 -0600

    Added Camera

--- app/src/main/java/com/example/sdgmentorshipapp/Schedule.java
+++ app/src/main/java/com/example/sdgmentorshipapp/Schedule.java
@@ -1,4 +1,24 @@
 package com.example.sdgmentorshipapp;
 
-public class Schedule {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.sdgmentorshipapp.ui.home.HomeViewModel;
+import com.google.android.material.navigation.NavigationView;
+
+public class Schedule extends AppCompatActivity {
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+    }
 }
